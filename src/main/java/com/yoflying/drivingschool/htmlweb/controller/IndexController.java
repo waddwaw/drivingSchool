ca4cf3665a0cd4b20321a8f91b17768a40149764
liqiang <waddwaw@qq.com>
2017-01-12 22:08:51 +0800

    加入判断是否为手机用户

--- src/main/java/com/yoflying/drivingschool/htmlweb/controller/IndexController.java
+++ src/main/java/com/yoflying/drivingschool/htmlweb/controller/IndexController.java
@@ -2,10 +2,13 @@ package com.yoflying.drivingschool.htmlweb.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by arvin on 2016/12/22.
@@ -14,8 +17,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class IndexController {
     private final Logger logger = LoggerFactory.getLogger(IndexController.class);
     @RequestMapping("/index")
-    public String index(ModelMap modelMap) {
+    public String index(HttpServletRequest request, ModelMap modelMap) {
         modelMap.put("index","欢迎使用 柚飞科技约车系统 ！开发人员 姚九龙 李娇 李立强");
+        
+        String ua = request.getHeader("user-agent");
+        //// TODO: 17/1/12 判断是否为手机用户
+        
         return "/index/index.ftl";
     }
 
