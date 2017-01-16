f88b07fc4a27f91ae0ae90e8400504a77b3c64c2
arvin <arvin@turingcat.com>
2017-01-16 15:21:04 +0800

    更新时间utils

--- src/main/java/com/yoflying/drivingschool/utils/json/TimeUtils.java
+++ src/main/java/com/yoflying/drivingschool/utils/json/TimeUtils.java
@@ -39,4 +39,31 @@ public class TimeUtils {
         }
         return ca.get(GregorianCalendar.AM_PM);
     }
+
+    /**
+     * 将日期转化为日期字符串。失败返回null。
+     * @param date 日期
+     * @param parttern 日期格式
+     * @return 日期字符串
+     */
+    public static String DateToString(Date date, String parttern) {
+        String dateString = null;
+        if (date != null) {
+            try {
+                dateString = getDateFormat(parttern).format(date);
+            } catch (Exception e) {
+            }
+        }
+        return dateString;
+    }
+
+    /**
+     * 获取SimpleDateFormat
+     * @param parttern 日期格式
+     * @return SimpleDateFormat对象
+     * @throws RuntimeException 异常：非法日期格式
+     */
+    private static SimpleDateFormat getDateFormat(String parttern) throws RuntimeException {
+        return new SimpleDateFormat(parttern);
+    }
 }
